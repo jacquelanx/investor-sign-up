55281cffa97045604ad1bb25de333664fd165c72
jacquelanx <109934084+jacquelanx@users.noreply.github.com>
2025-07-21 12:51:43 -0500

    Fixed lightningss according to Vercel doc

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import lightningcss from "vite-plugin-lightningcss";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    lightningcss({
+      browserslist: "last 2 versions",
+    }),
   ],
 })
