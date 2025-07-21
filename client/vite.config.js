70d8bbd86e193b4e20e969202ee99a42c1d90f3d
jacquelanx <109934084+jacquelanx@users.noreply.github.com>
2025-07-21 13:03:02 -0500

    Deleted lightningcss

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import lightningcss from "vite-plugin-lightningcss";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    lightningcss({
-      browserslist: "last 2 versions",
-    }),
   ],
 })
